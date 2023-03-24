94a350c95b24727df20dcee2f0e7db475d17bef8
Sistemas-Tecsellent <90065692+Sistemas-Tecsellent@users.noreply.github.com>
2023-03-24 10:27:38 -0600

    Added service to driver

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # Author: Angelo Arellano Gaona
 # Description: This program uses Selenium to scrap web pages and get prices for pokemon cards based on user search
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 
 def main():
     print("main")
-    driver = webdriver.Chrome(executable_path='./drivers/chromedriver.exe')
+    driver_service = Service(executable_path="/drivers/chromedriver.exe")
+    driver = webdriver.Chrome(service=driver_service)
     driver.get("https://google.com")
 
 
